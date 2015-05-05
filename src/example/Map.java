7c2e2f69daeaaecd66f819f88ec36a0378a0d0d7
arisvang <alexanderkurerisvang@gmail.com>
2015-05-05 22:20:04 +0200

    Class Map - Faulty integration.

--- src/example/Map.java
+++ src/example/Map.java
@@ -3,8 +3,6 @@ package example;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.Random;
-
-
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
@@ -17,34 +15,45 @@ public class Map{
 
 
 	public float[] mapGeneration (){
-		
-		int ScreenWidth = 480;
-		int kordinateLimit = 30;
-		
 		// The function random generates the map
+		int kordinateLimit = 30;
 		
 		// x starts in 0; then x+= random(limit,limit);
 		// y = random(limit,limit);
-		float[] trackArr = new float[kordinateLimit*4];  
+	float[] trackArr = new float[kordinateLimit*2]; 
+	
 		Random rnd = new Random();
+		
 		int i = 0;
-		for(int x = 0; x < ScreenWidth; x+= rnd.nextInt(20) + 5){ 
-			int y = rnd.nextInt(500)+100;
-			trackArr[i] = y;
-			trackArr[i+1] = y;
-			i+=2;
-		}
+		float xSum = 0;
+		
+		trackArr[0] = 0;
+		trackArr[1] = rnd.nextInt(400)+100;
+
+			for(int c = 2; c < 28; c++){
+				trackArr[i] = rnd.nextInt(20)+5 + xSum;
+				trackArr[i+1] = rnd.nextInt(400)+100;
+				xSum += trackArr[i];
+				if(trackArr[i] > 640){
+					trackArr[i] = 640; 
+				}// if()
+				i+=2;
+			}// for loop (c)
+		
+		System.out.println("KordinateOne: "+ trackArr[0] + " " + trackArr[1] + " " + "KordinateTwo: "+ trackArr[2] + " " + trackArr[3]);
+		
+		trackArr[58] = 640;
+		trackArr[59] = rnd.nextInt(400)+100;
 		
 		return trackArr;
-		// for loop (x = 0; if x < map.Width; x+= random(limit,limit){y = random(limit,limit);for(for(trackerArr[i,j] = trackerArr[x,y]))} 
-		// while creating the map every vector is tracked. Create 2D array that stores the kordinates.
+		
 		// track length of landing pads. if (x1 == x2), trackedLengthOne = x2-x1;
 		// if statements and counter has to make sure that 3 landing pads are made.
-	}
+	}// float[] mapGeneration.
 	
 	public void mapRenderer(Graphics g, float[] arr){
 		// Draw line between points.
-		for(int w = 0; w < arr.length-1;w+=4){
+		for(int w = 0; w < arr.length-3; w+=2){
 			g.drawLine(arr[w],arr[w+1],arr[w+2],arr[w+3]);
 		}
 		// Mark landing pads, based on length
--- src/example/SimpleSlickGame.java
+++ src/example/SimpleSlickGame.java
@@ -15,9 +15,11 @@ public class SimpleSlickGame extends BasicGame
 	}
 	
 	Map mapOne = new Map();
+	float[] mapArr = mapOne.mapGeneration();
 	
 	@Override
 	public void init(GameContainer gc) throws SlickException {
+		
 		// The init() method is only called ones
 		// this is were we place all the stuff needed for the game
 		// In our case where we create all the objects (player,map and so on).
@@ -36,7 +38,7 @@ public class SimpleSlickGame extends BasicGame
 	public void render(GameContainer gc, Graphics g) throws SlickException
 	{
 		// The render function updates the displayed graphics.
-		mapOne.mapRenderer(g, mapOne.mapGeneration());
+		mapOne.mapRenderer(g, mapArr);
 	}
 
 	public static void main(String[] args) // This function starts up the game.
