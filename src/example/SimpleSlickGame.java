975bf23942d19ee3a21608be1fa6a188351452c7
kevinpoulsen <kevinppoulsen@gmail.com>
2015-05-07 21:31:45 +0200

    Deleted cheat win function T

--- src/example/SimpleSlickGame.java
+++ src/example/SimpleSlickGame.java
@@ -7,7 +7,6 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.Input;
 import org.newdawn.slick.Music;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.Sound;
@@ -159,13 +158,6 @@ public class SimpleSlickGame extends BasicGame
 		// Same win condition, last landing pad.
 		if(padThreeBool == true && Player.angleState == 0 && Player.yCond < 5){
 			score+=Player.fuel/ GameMaster.timer;
-			gameState = 3;
-		}
-		
-		Input input; 
-		input = gc.getInput(); // listens for keyboard input
-		
-		if(input.isKeyPressed(Input.KEY_T)){
 			gameState = 3;
 		}
 		
