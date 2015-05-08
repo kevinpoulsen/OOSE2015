c7452659b97d80810deea9edb271d5cd747c3923
kevinpoulsen <kevinppoulsen@gmail.com>
2015-05-08 16:31:22 +0200

    Bling bling yall

--- src/example/GameMaster.java
+++ src/example/GameMaster.java
@@ -100,7 +100,9 @@ public class GameMaster {
 		space.draw(screenWidth-screenWidth,screenHeight-screenHeight);
 		
 		// Draws GUI based on screen width and height (Strings)
-		g.drawString("Insert Coin",(float) (screenWidth/2.5) , (float) (screenHeight/3));
+		if((int)(System.currentTimeMillis()/1000) % 2 == 0){
+		g.drawString("Insert Coin",(float) (screenWidth/2.5) , (float) (screenHeight/2.5));
+		}
 		g.drawString("Press Enter to continue",(float) (screenWidth/3.1) , (float) (screenHeight/2));
 		g.drawString("Gameplay info:",(float) (screenWidth/15) , (float) (screenHeight/1.22));
 		g.drawString("The aircraft has to land with a vertical speed below 5.",(float) (screenWidth/15) , (float) (screenHeight/1.15));
